63473a333952b7e7d0538b93b548a8265525b296
ayatsuki-meowmeow <aya.ayatsuki@gmail.com>
2026-02-12 22:20:31 +0900

    neovimの設定追加

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- share clipboard with OS
+vim.opt.clipboard:append("unnamedplus,unnamed")
+
+-- jj to quit insert mode
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+vim.api.nvim_create_user_command("InitLua", function()
+  vim.cmd.edit(vim.fn.stdpath("config") .. "/init.lua")
+end, {})
